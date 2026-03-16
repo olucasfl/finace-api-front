1fa600449e0976e15cec2450482d7f8e25996c0e
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-16 15:37:55 -0300

    feat: implement PWA services

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles/global.css";
-import "./styles/variables.css";
 
 import App from "./App";
 
