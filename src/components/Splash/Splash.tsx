1fc775c416e23de48db878037fb8655c22ee895a
Lucas Leandro <lucasfariasleandro@gmail.com>
2026-03-16 18:53:36 -0300

    fix: implement splash screen

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -4,18 +4,24 @@ import { isPWA } from "../../utils/isPwa"
 
 export default function Splash({children}:{children:React.ReactNode}){
 
- const [showSplash,setShowSplash] = useState(false)
+ const [showSplash,setShowSplash] = useState(true)
 
  useEffect(()=>{
 
   if(isPWA()){
-   setShowSplash(true)
 
    const timer = setTimeout(()=>{
+
     setShowSplash(false)
+
    },1800)
 
    return ()=>clearTimeout(timer)
+
+  }else{
+
+   setShowSplash(false)
+
   }
 
  },[])
@@ -28,29 +34,19 @@ export default function Splash({children}:{children:React.ReactNode}){
 
   <div className={styles.splash}>
 
-   {/* animated graph background */}
-
    <div className={styles.graph}>
-
     <span></span>
     <span></span>
     <span></span>
-
    </div>
 
-   {/* logo */}
-
    <div className={styles.logoContainer}>
-
     <img
      src="/logo-SF-blue-512.png"
      alt="Smart Finance"
      className={styles.logo}
     />
-
    </div>
-
-   {/* title */}
 
    <h1 className={styles.title}>
     SMART FINANCE
